b5be29a022879a3861538b13b3bb3ac9290d1b6e
Muganwa-rogers <umuganwarogers1@gmail.com>
2025-03-22 14:09:02 +0200

    16th commit

--- src/Components/AdBanner/AdBanner.jsx
+++ src/Components/AdBanner/AdBanner.jsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const gifs = [
-  "./Ads/apartments-for-sale-in-dubai-dubai-apartments.gif",
   "./Ads/bird-cctv.gif",
-  "./Ads/car-rental-in-port-louis-rent-a-car-in-port-louis (1).gif",
-  "./Ads/cars-sunday.gif",
-  "./Ads/caught-in-4k-caught-in-walton.gif",
-  "./Ads/japanese-mazda-premacy2016-japanese-used-car.gif",
-  "./Ads/sobha-projects-sobha-projects-bangalore.gif",
-  "./Ads/mauritius-car-rental.gif"
+  "./Ads/1.jpeg",
+  "./Ads/2.jpeg", 
+  "./Ads/spy-camera.gif",
+  "./Ads/3.jpeg",
+  "./Ads/4.jpeg",
+  "./Ads/Add a heading.png",
+  // "./Ads/car-rental-in-port-louis-rent-a-car-in-port-louis (1).gif",
+  // "./Ads/cars-sunday.gif",
+  // "./Ads/caught-in-4k-caught-in-walton.gif",
+  // "./Ads/japanese-mazda-premacy2016-japanese-used-car.gif",
+  // "./Ads/sobha-projects-sobha-projects-bangalore.gif",
+  // "./Ads/mauritius-car-rental.gif"
 ];
 
 const exitAnimations = [
@@ -35,12 +40,12 @@ const AdBanner = () => {
   }, []);
 
   return (
-    <div className="w-screen max-w-screen-xl m-auto mt-24 p-10 grid grid-cols-3 gap-4">
-      {[0, 1, 2].map((offset) => (
+    <div className="min-w-5xl m-auto mt-10 p-6 flex flex-row gap-2">
+      {[0, 1, 2, 3, 4].map((offset) => (
         <motion.img 
           key={offset} 
           src={gifs[(currentGif + offset) % gifs.length]} 
-          className="w-full h-96 object-cover" 
+          className="w-full h-48 object-contain rounded-lg shadow-lg" 
           alt="Advertisement Banner" 
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
